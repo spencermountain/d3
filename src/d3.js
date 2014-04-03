be8e4ca2a74017883429757d40ff99db83cf7c0c
Spencer Kelly <spencermountain@gmail.com>
2014-04-03 16:21:30 -0400

    reduce from 147k to 73k and keep barchart working

--- src/d3.js
+++ src/d3.js
@@ -1,23 +1,23 @@
 import "start";
-import "compat/";
+// import "compat/";
 
-import "arrays/";
-import "behavior/";
-import "color/";
+// import "arrays/";
+// import "behavior/";
+// import "color/";
 import "core/";
-import "dsv/";
+// import "dsv/";
 import "event/";
-import "format/";
-import "geo/";
-import "geom/";
-import "interpolate/";
-import "layout/";
-import "math/";
+// import "format/";
+// import "geo/";
+// import "geom/";
+// import "interpolate/";
+// import "layout/";
+// import "math/";
 import "scale/";
 import "selection/";
 import "svg/";
-import "time/";
+// import "time/";
 import "transition/";
-import "xhr/";
+// import "xhr/";
 
 import "end";
